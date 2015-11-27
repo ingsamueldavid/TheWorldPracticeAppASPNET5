1db5cff58117f1f4265ed0472e3aae04c1a2b35a
Samuel David De La Cruz <alphaelena@gmail.com>
2015-11-27 11:30:08 -0400

    initial commit

---
+++ src/TheWorldPracticeAppASPNET5/Controllers/Web/AppController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNet.Mvc;
+using System;
+using TheWorldPracticeAppASPNET5.ViewModels;
+
+namespace TheWorldPracticeAppASPNET5.Controllers.Web
+{
+    public class AppController:Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+
+            return View();
+        }
+
+
+    }
+
+
+
+
+}
