af9849bea10e9f5a687b1f632ba3139ca909b897
Samuel David De La Cruz <alphaelena@gmail.com>
2015-12-21 20:24:42 -0400

    added world user class

---
+++ src/TheWorldPracticeAppASPNET5/Models/WorldUser.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace TheWorldPracticeAppASPNET5.Models
+{
+    public class WorldUser : IdentityUser
+    {
+        public DateTime FirstTrip { get; set; }
+
+    }
+}
