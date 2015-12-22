116ba884d239ec1fffd659e02e0e94ff7c1090ab
Samuel David De La Cruz <alphaelena@gmail.com>
2015-12-21 20:28:22 -0400

    Added wild comment to worldUser class

--- src/TheWorldPracticeAppASPNET5/Models/WorldUser.cs
+++ src/TheWorldPracticeAppASPNET5/Models/WorldUser.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 
 namespace TheWorldPracticeAppASPNET5.Models
-{
+{//aqui un comentario salvaje
     public class WorldUser : IdentityUser
     {
         public DateTime FirstTrip { get; set; }
